30972f75fa219d41fffb769567d46aaf005252c3
genan2003 <genanomer99@gmail.com>
2024-07-20 23:35:50 +0300

    Second commit- creating book model

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 
@@ -7,6 +7,30 @@ const app = express();
 app.get('/', (request, response) =>{
     console.log(request);
     return response.status(234).send('Welcome!');
+});
+
+app.post('/books', async(request, response) => {
+    try{
+
+        if(!request.body.title || !request.body.author || !request.body.publishYear){
+            return response.status(400).send({
+                message: "Send all required fields: title, author, publishYear",
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
 

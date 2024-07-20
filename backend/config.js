c7a7d5b6620d840706a56bd2aeef42b0a00e4e71
genan2003 <genanomer99@gmail.com>
2024-07-20 23:21:11 +0300

    First commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL= 'mongodb+srv://genanomer99:7ddFIYyolzk9iwVY@mern-bookstoreapp.jjsx5qh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERN-BookStoreApp';
---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get('/', (request, response) =>{
+    console.log(request);
+    return response.status(234).send('Welcome!');
+});
+
+
+mongoose.connect(mongoDBURL).then(() =>{
+    console.log("App is connected to the database");
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+    });
+}). catch((error) =>{
+    console.log(error);
+});
